39f4410aeb08f5828c4b2c4610b036581a3999a5
Gabriel Lando <glando.py@gmail.com>
2023-07-27 20:27:36 +0300

    Create template for checkout SPA

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <title>PHISHED STORE</title>
+
+        <link rel="stylesheet" href="">
+
+    </head>
+
+    <body class="antialiased">
+        <div class="container mx-auto">
+            <div id="app"></div>
+        </div>
+
+    </body>
+</html>
