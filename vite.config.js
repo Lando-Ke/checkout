4a8d01f7e2e401bb6a9c09b269322da6351012ae
Gabriel Lando <glando.py@gmail.com>
2023-07-27 21:02:39 +0300

    Setup and configure tailwind

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: '0.0.0.0',
+        },
+        watch: {
+            usePolling: true
+        }
+    },
 });
